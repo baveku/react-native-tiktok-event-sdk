34220b400dfb75f81fc1b5f38b52b89671066c62
bachvq <baveku@gmail.com>
2025-09-29 15:50:19 +0700

    implement android

--- android/src/main/java/com/tiktokeventsdk/HybridTiktokEventSdk.kt
+++ android/src/main/java/com/tiktokeventsdk/HybridTiktokEventSdk.kt
@@ -1,9 +1,54 @@
 package com.tiktokeventsdk
 
+import com.margelo.nitro.core.Promise
 import com.margelo.nitro.tiktokeventsdk.HybridTiktokEventSdkSpec
+import com.tiktok.TikTokBusinessSdk.*;
+import com.tiktok.TikTokBusinessSdk.TTConfig;
+import com.tiktok.TikTokBusinessSdk.TTInitCallback
+import com.tiktok.appevents.base.TTBaseEvent
 
-class HybridTiktokEventSdk: HybridTiktokEventSdkSpec() {    
-    override fun sum(num1: Double, num2: Double): Double {
-        return num1 + num2
+class CallBack: TTInitCallback {
+    override fun success() {
+        TODO("Not yet implemented")
+    }
+
+    override fun fail(code: Int, msg: String?) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class HybridTiktokEventSdk: HybridTiktokEventSdkSpec() {
+    override fun initialSdk(accessToken: String, appId: String, tiktokAppId: String): Promise<Boolean> {
+        val prs = Promise<Boolean>()
+        val config = TTConfig(getApplicationContext(), accessToken).setAppId(appId).setTTAppId(tiktokAppId).enableAutoIapTrack()
+        initializeSdk(config, object: TTInitCallback {
+            override fun success() {
+                prs.resolve(true)
+            }
+
+            override fun fail(code: Int, msg: String?) {
+                prs.reject(Throwable(msg))
+            }
+
+        })
+        return prs
+    }
+
+    override fun identifyUser(userId: String, userName: String?, phone: String?, email: String?) {
+        identifyUser(userId, userName, phone, email)
+    }
+
+    override fun logout() {
+        logout()
+    }
+    
+    override fun trackEvent(event: String, params: Map<String, String>?) {
+        if (params == null) return
+        val eventBuilder = TTBaseEvent.newBuilder(event)
+        params.keys.forEach {
+            eventBuilder.addProperty(it, params[it])
+        }
+        trackTTEvent(eventBuilder.build())
     }
 }
